3610bcf3aa9b3fd6df0df1da39f3fb59f24dda7c
DanielHalachev <dani22h09_pl@abv.bg>
2019-11-03 14:11:48 +0200

    Swagger SetUp

---
+++ TNDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,57 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TNDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {   
+            //Adds a new route 
+            // Добавя нов път
+            swaggerDoc.paths.Add("/token", new PathItem 
+            {
+                post = new Operation //POST
+                {                    
+                    tags = new List<string> { "Auth" }, //"Auth" category //Категория                    
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded" //Параметрите идват оттук
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter 
+                        {
+                            type="string",
+                            name="grant_type",
+                            required=true,
+                            @in="formData",
+                            @default="password"
+                        },
+                        //Потребителско име
+                        new Parameter
+                        {
+                            type="string",
+                            name="username",
+                            required=false,
+                            @in="formData"
+                        },
+                        //Парола
+                        new Parameter
+                        {
+                            type="string",
+                            name="password",
+                            required=false,
+                            @in="formData"
+                        }
+                    }
+                }
+            }); 
+
+        }
+    }
+}
