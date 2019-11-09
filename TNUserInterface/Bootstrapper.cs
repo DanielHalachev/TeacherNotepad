fce18de338303db52683dfe1ff9dbe96f28fd7e7
DanielHalachev <dani22h09_pl@abv.bg>
2019-11-09 20:16:59 +0200

    Dependency Bootstrapper and ViewModel wire-up LogInForm basic setup.

--- TNUserInterface/Bootstrapper.cs
+++ TNUserInterface/Bootstrapper.cs
@@ -11,13 +11,39 @@ namespace TNUserInterface
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
+        protected override void Configure()
+        {
+            _container.Instance(_container);//when you ask for a SimpleContainer Instance, it returns this instance
+            _container.Singleton<IWindowManager, WindowManager>()
+                      .Singleton<IEventAggregator, EventAggregator>();//Allows for events to be used in all different parts (views) of the App
+            GetType().Assembly.GetTypes()//get all types in the app, which:
+                .Where(type => type.IsClass)//are Class-types
+                .Where(type => type.Name.EndsWith("ViewModel"))//and the names of the Classes end in "ViewModel"
+                .ToList() //Create a List of those classes
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));//For each "ViewModel" class does "_container.RegisterPerRequest"
+        }
+        //The following code applies Bootstrapper methods to our ViewModels through overriding
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            DisplayRootViewFor<ShellViewModel>();
+            DisplayRootViewFor<ShellViewModel>();//Shows view in ShellViewModel
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
---
+++ TNUserInterface/ViewModels/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNUserInterface.ViewModels
+{
+    public class LoginViewModel: Screen
+    {
+        private string _userName;//underscore means it's just a backing field that holds the value and passes it to the property
+        private string _password;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);//works with properties, not fields. That's why I am using it with the property
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+        public bool CanLogIn(string userName, string password)
+        {
+            if(userName.Length>0&& password.Length > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void LogIn(string userName, string password)
+        {
+            Console.WriteLine();
+        }
+    }
+}
